67ac6614da7e4cd0f2256decd44e5b48b94ccfb4
Seleisha <96621290+woerlong@users.noreply.github.com>
2022-03-24 09:41:01 +0800

    Initial commit

---
+++ src/main/java/com/main/User.java
@@ -0,0 +1,40 @@
+package com.main;
+
+public class User {
+    int id;
+    String username;
+    String userpsw;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + userpsw + '\'' +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return userpsw;
+    }
+
+    public void setPassword(String userpsw) {
+        this.userpsw = userpsw;
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,24 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: 000
+  Date: 2022/3/24
+  Time: 8:40
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <title>用户登录</title>
+</head>
+
+<body>
+<form action="请求路径" method="post" id="myform">
+  <input type="text" id="username" name="username"/>
+  <input type="password" id="password" name="password"/>
+  <input type="submit" value="提交" id="login" />
+</form>
+</body>
+
+</html>
