2ef641ce918dcef63ae23347351832f364eb90a3
YukinaSor4 <1649290871@qq.com>
2022-04-06 00:00:03 +0800

    完成框架搭建

--- src/main/java/com/controller/ChatController.java
+++ src/main/java/com/controller/ChatController.java
@@ -23,7 +23,7 @@ public class ChatController {
     @RequestMapping("/newchat")
     public String newChat(Chat chat){
         System.out.println(chat);
-        chatService.Addtime(chat);
+
         chatService.newChat(chat);
         return "你妈的";
     }
